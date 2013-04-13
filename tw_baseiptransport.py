266823aea3c10c86f9daa69f6a8a569e080f4a23
pallenpost@gmail.com <pallenpost@gmail.com@d34a8d7b-fcd9-cb39-a4a8-77dc4d531bb8>
2013-04-13 18:56:00 +0000

    Updated Twisted's license and copyright details

--- tw_baseiptransport.py
+++ tw_baseiptransport.py
@@ -4,9 +4,30 @@
 
 # Most of this code was lifted out of Twisted's twisted.internet.udp.Port
 # class, with minor modifications to make it work for raw IP sockets instead
-# of UDP sockets. Copyright notice from Twisted:
+# of UDP sockets. Copyright notice and license from Twisted:
 #
-# Copyright (c) Twisted Matrix Laboratories.
+# Twisted, the Framework of Your Internet
+# Copyright (c) 2001-2011 Twisted Matrix Laboratories.
+# See LICENSE for details.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import struct
 import socket
