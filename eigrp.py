4c1ff99130339f66a5ba72cf07a2c64f73b75c25
pallenpost@gmail.com <pallenpost@gmail.com@d34a8d7b-fcd9-cb39-a4a8-77dc4d531bb8>
2013-04-13 18:36:35 +0000

    1. Added more startup and cleanup routines 2. Added RTPPacket class and RTPHeader checksumming 3. Added opcode handler stubs

--- eigrp.py
+++ eigrp.py
@@ -6,9 +6,11 @@ import logging
 import logging.config
 import functools
 import struct
+import binascii
 from twisted.internet import reactor
 from twisted.internet import protocol
 from twisted.python import log
+import ipaddr
 
 import tw_baseiptransport
 import sysiface
@@ -16,7 +18,7 @@ import util
 
 class EIGRP(protocol.DatagramProtocol):
     def __init__(self, rid, asn, routes, import_routes, interfaces, log_config,
-                 admin_port):
+                 admin_port, hdrver=2):
         """An EIGRP implementation based on Cisco's draft informational RFC
         located here:
 
@@ -25,20 +27,31 @@ class EIGRP(protocol.DatagramProtocol):
         rid -- The router ID to use
         asn -- The autonomous system number
         """
-        input_err_msg = "%s must be a positive number less than 65536."
+        asn_rid_err_msg = "%s must be a positive number less than 65536."
         if not isinstance(rid, int):
-            raise(TypeError(input_err_msg % "Router ID"))
+            raise(TypeError(asn_rid_err_msg % "Router ID"))
         if not (0 <= rid < 65536):
-            raise(ValueError(input_err_msg % "Router ID"))
+            raise(ValueError(asn_rid_err_msg % "Router ID"))
         if not isinstance(asn, int):
-            raise(TypeError(input_err_msg % "AS Number"))
+            raise(TypeError(asn_rid_err_msg % "AS Number"))
         if not (0 <= asn < 65536):
-            raise(ValueError(input_err_msg % "AS Number"))
+            raise(ValueError(asn_rid_err_msg % "AS Number"))
         self._rid = rid
         self._asn = asn
-        self._hdrver = 2
+
+        if hdrver == 2:
+            self._rtphdr = RTPHeader2
+        else:
+            raise(ValueError("Unsupported header version: %d" % hdrver))
 
         self._init_logging(log_config)
+        if sys.platform == "linux2":
+            self._sys = sysiface.LinuxSystem(log_config=log_config)
+        elif sys.platform == "win":
+            self._sys = sysiface.WindowsSystem(log_config=log_config)
+        else:
+            raise(NotSupported("No support for current OS."))
+        self._register_op_handlers()
         #eigrpadmin.run(self, port=admin_port)
 
     def _init_logging(self, log_config):
@@ -56,27 +69,160 @@ class EIGRP(protocol.DatagramProtocol):
         suppress_reactor_not_running = functools.partial(util.suppress_reactor_not_running, logfunc=self.log.debug)
         log.addObserver(suppress_reactor_not_running)
 
+    def _register_op_handlers(self):
+        self._op_handlers = dict()
+        self._op_handlers[self._rtphdr.OPC_UPDATE] = self._eigrp_packet_handler_update
+        self._op_handlers[self._rtphdr.OPC_REQUEST] = self._eigrp_packet_handler_request
+        self._op_handlers[self._rtphdr.OPC_QUERY] = self._eigrp_packet_handler_query
+        self._op_handlers[self._rtphdr.OPC_REPLY] = self._eigrp_packet_handler_reply
+        self._op_handlers[self._rtphdr.OPC_HELLO] = self._eigrp_packet_handler_hello
+        self._op_handlers[self._rtphdr.OPC_SIAQUERY] = self._eigrp_packet_handler_siaquery
+        self._op_handlers[self._rtphdr.OPC_SIAREPLY] = self._eigrp_packet_handler_siareply
+
+    def _eigrp_packet_handler_update(self, addr, hdr, data):
+        self.log.debug("Processing UPDATE")
+
+    def _eigrp_packet_handler_request(self, addr, hdr, data):
+        self.log.debug("Processing REQUEST")
+
+    def _eigrp_packet_handler_query(self, addr, hdr, data):
+        self.log.debug("Processing QUERY")
+
+    def _eigrp_packet_handler_reply(self, addr, hdr, data):
+        self.log.debug("Processing REPLY")
+
+    def _eigrp_packet_handler_hello(self, addr, hdr, data):
+        self.log.debug("Processing HELLO")
+
+    def _eigrp_packet_handler_siaquery(self, addr, hdr, data):
+        self.log.debug("Processing SIAQUERY")
+
+    def _eigrp_packet_handler_siareply(self, addr, hdr, data):
+        self.log.debug("Processing SIAREPLY")
+
     def run(self):
         reactor.listenIP(reactor, 88, self)
+        self.log.info("EIGRP is starting up...")
         reactor.run()
+
+    def _cleanup(self):
+        # XXX Add cleanup for routes when we have any to remove
+        self.log.info("Cleaning up.")
+        self._sys.cleanup()
 
     def startProtocol(self):
         pass
 
+    def stopProtocol(self):
+        self.log.info("EIGRP is shutting down.")
+        self._cleanup()
+
     def datagramReceived(self, data, addr_and_zero):
         addr = addr_and_zero[0]
+        host_local = False
+        addr = ipaddr.IPv4Address(addr)
+        for local_iface in self._sys.logical_ifaces:
+            if local_iface.ip.ip.exploded == addr.exploded:
+                host_local = True
+                break
+        if host_local:
+            self.log.debug5("Ignoring message from local system.")
+            return
+
+        self.log.debug("Received datagram from %s" % addr)
         try:
-            hdr = RTPHeader(data[:RTPHeader.HEADERLEN])
+            hdr = self._rtphdr(data[:self._rtphdr.HEADERLEN])
         except struct.error:
-            pass
-        print "Received datagram from %s" % addr
+            bytes_to_print = self._rtphdr.HEADERLEN
+            self.log.warn("Received malformed datagram from %s. Hexdump of "
+                          "first %d bytes: %s" % (addr, bytes_to_print, \
+                          binascii.hexlify(data[:bytes_to_print])))
+            return
+        if hdr.hdrver != self._rtphdr.VER:
+            self.log.warn("Received incompatible header version %d from "
+                          "host %s" % (hdr.hdrver, addr))
+            return
+
+        # XXX RTP, ND/NR processing here
+
+        payload = data[self._rtphdr.HEADERLEN:]
+        try:
+            handler = self._op_handlers[hdr.opcode]
+        except KeyError:
+            self.log.info("Received invalid/unhandled opcode %d from %s" % \
+                          (hdr.opcode, addr))
+            return
+        handler(addr, hdr, payload)
 
 
-class RTPHeader(object):
-    """Reliable Transport Protocol Header."""
+class EIGRPException(Exception):
+    def __init__(self, msg=""):
+       self.msg = msg
+
+
+class NotSupported(EIGRPException):
+    def __init__(self, *args, **kwargs):
+        super(EIGRPException, self).__thisclass__.__init__(self, *args, **kwargs)
+
+class StubFields(object):
+    """Stub class to test checksum calculation"""
+    def pack(self):
+        return "hi"
+
+
+class RTPPacket(object):
+    def __init__(self, hdr, fields):
+        self.hdr = hdr
+        self.fields = fields
+
+    def pack(self):
+        self.hdr.chksum = 0
+        prehdr = self.hdr.pack()
+        fields = self.fields.pack()
+        self.hdr.chksum = self.calc_chksum(prehdr, fields)
+        hdr = self.hdr.pack()
+        return hdr + fields
+
+    # Checksum related functions are from:
+    # http://stackoverflow.com/questions/1767910/checksum-udp-calculation-python
+    @staticmethod
+    def calc_chksum(prehdr, fields):
+        """Get one's complement of the one's complement sum. Covers header
+        fields and the TLVs.
+        prehdr should be the packed header, with checksum set to 0
+        fields should be the packed TLV fields (not in a list)
+        Returns the 16 bit checksum.
+        """
+        data = prehdr + fields
+        data = map(lambda x: ord(x), data)
+        data = struct.pack("%dB" % len(data), *data)
+        s = 0
+        for i in range(0, len(data), 2):
+            w = ord(data[i]) + (ord(data[i+1]) << 8)
+            s = RTPPacket.carry_around_add(s, w)
+        return ~s & 0xffff
+
+    @staticmethod
+    def carry_around_add(a, b):
+        c = a + b
+        return (c & 0xffff) + (c >> 16)
+
+
+class RTPHeader2(object):
+    """Reliable Transport Protocol Header (header version 2)."""
 
     FORMAT = "BBHIIIHH"
     HEADERLEN = struct.calcsize(FORMAT)
+    VER = 2
+
+    OPC_UPDATE   = 1
+    OPC_REQUEST  = 2
+    OPC_QUERY    = 3  # Should this be 4? RFC says 4 but might be a typo.
+    OPC_REPLY    = 4
+    OPC_HELLO    = 5
+    OPC_PROBE    = 7
+    OPC_SIAQUERY = 10
+    OPC_SIAREPLY = 11
 
     def __init__(self, raw=None, hdrver=None, opcode=None, flags=None,
                  seq=None, ack=None, rid=None, asn=None):
@@ -97,25 +243,26 @@ class RTPHeader(object):
              ack    != None and \
              rid    != None and \
              asn    != None:
-            self._hdrver = hdrver
-            self._opcode = opcode
-            self._flags = flags
-            self._seq = seq
-            self._ack = ack
-            self._rid = rid
-            self._asn = asn
+            self.hdrver = hdrver
+            self.opcode = opcode
+            self.flags = flags
+            self.seq = seq
+            self.ack = ack
+            self.rid = rid
+            self.asn = asn
+            self.chksum = 0
         else:
             raise(ValueError("Either 'raw' is required, or all other arguments"
                              " are required."))
 
     def unpack(self, raw):
-        self._raw, self._hdrver, self._opcode, self._flags, self._seq, \
-             self._ack, self._rid, self._asn = struct.unpack(self.FORMAT, raw)
+        self.hdrver, self.opcode, self.chksum, self.flags, self.seq, \
+             self.ack, self.rid, self.asn = struct.unpack(self.FORMAT, raw)
 
     def pack(self):
-        return struct.pack(self.FORMAT, self._hdrver, self._opcode,
-                           self._flags, self._seq, self._ack, self._rid,
-                           self._asn)
+        return struct.pack(self.FORMAT, self.hdrver, self.opcode, self.chksum,
+                           self.flags, self.seq, self.ack, self.rid,
+                           self.asn)
 
 def parse_args(argv):
     op = optparse.OptionParser()
--- test/send_test_pkt.py
+++ test/send_test_pkt.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
 import socket
+import sys
+sys.path.append("..")
+
+import eigrp
 
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, 88)
-s.sendto("hi", ("127.0.0.1", 0))
+hdr = eigrp.RTPHeader2(hdrver=2, opcode=10, flags=0, seq=0, ack=0, rid=0, asn=0)
+fields = eigrp.StubFields()
+pkt = eigrp.RTPPacket(hdr, fields)
+s.sendto(pkt.pack(), ("127.0.0.1", 0))
