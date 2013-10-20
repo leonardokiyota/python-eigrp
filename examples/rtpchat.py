b5fdea1bc3c720ccbf0c108c9f68ede8967ccf1f
pallenpost@gmail.com <pallenpost@gmail.com@d34a8d7b-fcd9-cb39-a4a8-77dc4d531bb8>
2013-10-20 01:30:54 +0000

    1. Basic Tkinter gui functionality is working for RTP chat

--- examples/rtpchat.py
+++ examples/rtpchat.py
@@ -3,10 +3,14 @@
 """A chat client that finds link-local users using RTP. Used to demonstrate
 that this implementation of RTP is decoupled from EIGRP."""
 
+import Tkinter
 import sys
+from twisted.internet import tksupport
+
 sys.path.append("..")
 
 from tw_baseiptransport import reactor
+import tw_baseiptransport
 import rtp
 import rtptlv
 import util
@@ -20,10 +24,12 @@ class _BaseRTPChatGUI(object):
     serves as a programming inteface that should be overriden by the actual
     GUI class."""
 
-    def __init__(self, sendfunc):
+    def __init__(self, sendfunc, quitfunc):
         """sendfunc -- The function that the GUI should call when the
-                       GUI has text to send."""
+                       GUI has text to send.
+           quitfunc -- A function to call when a user quits the application"""
         self._send = sendfunc
+        self._quit = quitfunc
 
     def receive_text(self, neighbor, text):
         """Called when a neighbor has sent us a text message."""
@@ -33,12 +39,90 @@ class _BaseRTPChatGUI(object):
         when we first discover a neighbor and obtain its username.)"""
 
 
-class RTPChatGtkGUI(_BaseRTPChatGUI):
-    def receive_text(self, neighbor, text):
-        print("*** GUI got text")
+class RTPChatTkinterGUI(_BaseRTPChatGUI):
 
-    def update_username(self, neighbor, text):
-        print("*** GUI got updated username")
+    def __init__(self, *args, **kwargs):
+        _BaseRTPChatGUI.__init__(self, *args, **kwargs)
+        self._init_gui()
+        self._neighbor_indexes = list()
+
+    def _init_gui(self):
+        self._root = Tkinter.Tk()
+        tksupport.install(self._root)
+        self._root.title("RTPChat Tkinter GUI")
+        self._root.geometry("575x685")
+
+        self._frame_main = Tkinter.Frame(self._root)
+        self._frame_main.grid()
+
+        self._frame_messages = Tkinter.LabelFrame(self._frame_main,
+                                                  text="Message Log")
+        self._frame_messages.grid()
+        self._txt_messages = Tkinter.Text(self._frame_messages)
+        self._txt_messages.grid()
+        self._txt_messages.config(state=Tkinter.DISABLED)
+
+        self._frame_neighbors = Tkinter.LabelFrame(self._frame_main,
+                                                   text="Neighbors List")
+        self._frame_neighbors.grid()
+
+        self._lst_neighbors = Tkinter.Listbox(self._frame_neighbors,
+                                              selectmode=Tkinter.SINGLE)
+        self._lst_neighbors.grid(row=1, column=1)
+
+        self._frame_input = Tkinter.LabelFrame(self._frame_main,
+                                               text="Input")
+        self._frame_input.grid()
+        self._var_input = Tkinter.StringVar()
+        self._txt_input = Tkinter.Entry(self._frame_input,
+                                        textvariable=self._var_input)
+        self._txt_input.grid(row=2, column=1)
+
+        self._btn_send = Tkinter.Button(self._frame_main, text="Send",
+                                        command=self._send_text)
+        self._btn_send.grid(row=3)
+
+        self._btn_quit = Tkinter.Button(self._frame_main, text="Quit",
+                                        command=self._confirm_quit)
+        self._btn_quit.grid(row=4)
+        self._write_local_msg("RTP Chat has started. Usage:")
+        self._write_local_msg("When a neighbor RTP Chat client comes online, it will appear in the Neighbors list below.")
+        self._write_local_msg("Click on a neighbor then type in the Input box to send them a message.")
+        self._write_local_msg("Messages that you receive from neighbors will appear in this window.")
+        self._write_local_msg("This is really only intended to test out RTP, so don't be surprised if RTP Chat does something bad.")
+
+    def _write_local_msg(self, msg):
+        """Write a message to self._txt_messages. Make _txt_messages writable
+        only while we are using it, so it appears to be read-only to
+        operators."""
+        self._txt_messages.config(state=Tkinter.NORMAL)
+        self._txt_messages.insert(Tkinter.END, msg + "\n")
+        self._txt_messages.config(state=Tkinter.DISABLED)
+
+    def _confirm_quit(self):
+        # TODO confirm that the operator wants to quit...
+        self._quit()
+
+    def _send_text(self):
+        """Send the text that was entered in the input Entry widget to the
+        selected neighbor, then clear the input Entry widget."""
+        neighbor_index = self._lst_neighbors.index(Tkinter.ACTIVE)
+        if neighbor_index == None:
+            self._write_local_msg("You must select a neighbor to talk to before sending a message.")
+            return
+
+        neighbor = self._neighbor_indexes[neighbor_index]
+        self._send(neighbor, self._var_input.get())
+        self._var_input.set('')
+
+    def receive_text(self, neighbor, text):
+        self._write_local_msg(neighbor._username + " wrote to you: \"" + text.text + "\"")
+
+    def update_username(self, neighbor, username):
+        # Keep track of which neighbor is at each index in the
+        # neighbor listbox.
+        self._lst_neighbors.insert(Tkinter.END, username)
+        self._neighbor_indexes.append(neighbor)
 
 
 class ValueText(rtptlv.ValueBase):
@@ -116,7 +200,8 @@ class RTPChat(rtp.ReliableTransportProtocol):
         rtp.ReliableTransportProtocol.__init__(self, *args, **kwargs)
         self.activate_iface(ip)
         if ui == self.GTK_UI:
-            self._ui = RTPChatGtkGUI(self._send_chat_msg)
+            self._ui = RTPChatTkinterGUI(self._send_chat_msg,
+                                         reactor.stop)
         else:
             raise(ValueError("Unsupported GUI type: {}".format(ui)))
         self._username = username
@@ -137,12 +222,13 @@ class RTPChat(rtp.ReliableTransportProtocol):
                 self.log.debug("Unknown reply TLV: {}".format(tlv))
 
     def _update_username(self, neighbor, text):
-        self.log.debug("Updating neighbor {} to use username {}".format(neighbor, text))
+        self.log.debug("Updating neighbor {} to use username " \
+                       "{}".format(neighbor, text))
         neighbor._username = text
         self._ui.update_username(neighbor, text)
 
     def _process_chat_msg(self, neighbor, text):
-        self._ui.receive_text(neighbor)
+        self._ui.receive_text(neighbor, text)
 
     def _send_username(self, neighbor):
         """Send our username to a neighbor."""
@@ -161,7 +247,7 @@ class RTPChat(rtp.ReliableTransportProtocol):
 
     def _send_chat_msg(self, neighbor, text):
         tlvs = [TLVText(text)]
-        self.neighbor.send(self._rtphdr.OPC_REPLY, tlvs, True)
+        neighbor.send(self._rtphdr.OPC_REPLY, tlvs, True)
 
     def initReceived(self, neighbor):
         pass
@@ -208,6 +294,7 @@ def main(args):
     system = sysiface.SystemFactory(0, 0).build()
     rtpchat = RTPChat(user, RTPChat.GTK_UI, ip, system=system,
                       logconfig=logconfig)
+    
     rtpchat.run()
 
 if __name__ == "__main__":
